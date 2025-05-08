d7d8a348816e76c862988917c0ec64323e87afa3
Nadya <msyoo1682@gmail.com>
2025-05-08 16:04:27 +0300

    all

--- scr/cli.js
+++ scr/cli.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
-
 const userName = () => {
   const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
+  return console.log(`Hello, ${name}!`);
 };
-export default userName();
+
+export default userName;
+
