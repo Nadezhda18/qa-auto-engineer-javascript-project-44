b42055a7bb2d532eca34147a25747b7971c620ec
Nadya <msyoo1682@gmail.com>
2025-05-09 10:19:30 +0300

    bin/brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -7,31 +7,23 @@ const name = userName();
 
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-let numberOfCorrectAnsf = 0;
 
 const brainEven = () => {
-    const a = Math.random(); 
-    const number = readlineSync.question('Question: ' + a);
-    const answer = readlineSync.question('Your answer: '); 
-    for (let numberOfCorrectAnsf = 0; numberOfCorrectAnsf < 4; numberOfCorrectAnsf += 1) {
-        if (number % 2 !== 0 && answer === 'no') {
-            numberOfCorrectAnsf += 1;
-            return console.log('Correct!');
-        } else if (number % 2 !== 0 && answer === 'yes') {
-            return console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
-        } else if (number % 2 === 0 && answer === 'yes') {
-            numberOfCorrectAnsf += 1;
-            return console.log('Correct!');
-        } else if (number % 2 === 0 && answer === 'no') {
-            return console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${name}!`);
-        } 
+  const a = Math.random();
+  const number = readlineSync.question(`Question: ${a}`);
+  const answer = readlineSync.question('Your answer: ');
+  for (let numberOfCorrectAnsf = 0; numberOfCorrectAnsf < 4; numberOfCorrectAnsf += 1) {
+    if (number % 2 !== 0 && answer === 'no') {
+      numberOfCorrectAnsf += 1;
+      return console.log('Correct!');
+    } if (number % 2 !== 0 && answer === 'yes') {
+      return console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
+    } if (number % 2 === 0 && answer === 'yes') {
+      numberOfCorrectAnsf += 1;
+      return console.log('Correct!');
+    } if (number % 2 === 0 && answer === 'no') {
+      return console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${name}!`);
     }
-    return numberOfCorrectAnsf;
-    
+  }
 };
-
-if (numberOfCorrectAnsf === 3) {
-    return console.log(`Congratulations, ${name}!`)
-}
-
 export default brainEven;
