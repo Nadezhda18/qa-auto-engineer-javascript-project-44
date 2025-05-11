3218f324bc0836f96f641107e96ab06281cd1251
Nadya <msyoo1682@gmail.com>
2025-05-11 10:32:51 +0300

    bin/brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -3,16 +3,25 @@ import readlineSync from 'readline-sync';
 import userName from '../scr/cli.js';
 
 const getRandomNumber = (max, min) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+  return randomNumber;
+};
+
+const getRandomSym = (elements) => {
+  const randomSymbol = elements[Math.floor(Math.random() * elements.length)];
+  return randomSymbol;
+};
+
+const symbols = ['+', '-', '*'];
 
 const brainCalc = (name) => {
   const 
   console.log('What is the result of the expression?');
   let numberOfCorrectAnsf = 0;
   while (numberOfCorrectAnsf !== 3) {
-    const a = Math.floor((Math.random() * 100));
-    console.log(`\nQuestion: ${a} `);
+    const number = getRandomNumber(0, 100);
+    const symbol = getRandomSym(symbols);
+    console.log(`\nQuestion: ${number} ${symbol} ${number}`);
     const answer = readlineSync.question('Your answer: ');
   }
 
