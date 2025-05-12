4e4b3e075b24588b27208a360a5f47f5f576ea6a
Nadya <msyoo1682@gmail.com>
2025-05-12 14:37:35 +0300

    bin/brain-progression.js

---
+++ bin/brain-progression.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import userName from '../scr/cli.js';
+
+const getRandomNumber = (min, max) => {
+  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+  return randomNumber;
+};
+
+const progression = (name) => {
+  console.log('What number is missing in the progression?');
+  let numberOfCorrectAnsf = 0;
+  while (numberOfCorrectAnsf !== 3) {
+    const seriesOfNumbers = [];
+    const diffOfProgr = getRandomNumber(1, 10);
+    for (let i = 0; i <= getRandomNumber(5, 11); i += 1) {
+      seriesOfNumbers[i] = getRandomNumber(0, 100) + diffOfProgr;
+    }
+    const hiddenNumber = getRandomNumber(0, progression.lenght);
+    const correctAnswer = seriesOfNumbers[hiddenNumber];
+    seriesOfNumbers[hiddenNumber] = '..';
+    console.log(`\nQuestion: ${seriesOfNumbers}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+      numberOfCorrectAnsf += 1;
+    }
+    if (answer !== correctAnswer) {
+      (console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.\nLet's try again, ${name}!`));
+    }
+  } return console.log(`Congratulations, ${name}!`);
+};
+
+export default progression;
+
+progression(userName());
