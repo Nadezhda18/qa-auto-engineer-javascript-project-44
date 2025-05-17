6790e604e086c992a4eb5e0224ae51f0f5a4d18a
Nadya <msyoo1682@gmail.com>
2025-05-17 11:33:54 +0300

    bin/brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import userName from '../src/cli.js'
-import { brainEven } from '../src/index.js'
+import { brainEven } from '../games/brain-even_game.js'
 
-brainEven(userName())
+brainEven()
