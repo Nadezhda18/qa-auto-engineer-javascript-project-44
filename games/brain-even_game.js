c2eda05e35b5f672c3b9b0f1f687401fcb45ca22
Nadya <msyoo1682@gmail.com>
2025-05-17 11:33:07 +0300

    games/brain-even_game.js

--- games/brain-even_game.js
+++ games/brain-even_game.js
@@ -1,35 +1,30 @@
-import readlineSync from 'readline-sync'
+import { gamesStructureForNumb } from '../src/index.js'
 
 const getRandomNumber = (max, min) => {
   const b = Math.floor(Math.random() * (max - min + 1)) + min
   return b
 }
-export { getRandomNumber }
 
-const brainEven = (name) => {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".')
-  let numberOfCorrectAnsf = 0
-  while (numberOfCorrectAnsf !== 3) {
-    const a = getRandomNumber(0, 100)
-    console.log(`\nQuestion: ${a} `)
-    let answer = readlineSync.question('Your answer: ')
-    if (answer !== 'no' && answer !== 'yes') {
-      console.log(`${answer} is wrong answer ;(. Your answer can only be 'yes' or 'no'.\nLet's try again, ${name}!`)
-      answer = readlineSync.question('Your answer: ').toLowerCase().trim()
-    }
-    if (a % 2 !== 0 && answer === 'no') {
-      console.log('Correct!')
-      numberOfCorrectAnsf += 1
-    }
-    if (a % 2 === 0 && answer === 'yes') {
-      console.log('Correct!')
-      numberOfCorrectAnsf += 1
-    }
-    if ((a % 2 !== 0 && answer === 'yes') || (a % 2 === 0 && answer === 'no')) {
-      console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`)
-      return
-    }
+const questionOfGame = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+const gameProcess = () => {
+  const a = getRandomNumber(0, 100)
+  const question = a
+  let correctAnswer
+  if (a % 2 !== 0) {
+    correctAnswer = 'no'
   }
-  console.log(`Congratulations, ${name}!`)
+  if (a % 2 === 0) {
+    correctAnswer = 'yes'
+  }
+  return {
+    question,
+    correctAnswer,
+  }
 }
+
+const brainEven = () => {
+  gamesStructureForNumb(questionOfGame, gameProcess)
+}
+
 export { brainEven }
