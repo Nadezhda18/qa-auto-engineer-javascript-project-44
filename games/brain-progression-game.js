e655f46c41abbaed41a66ff32bb15d98bc5715e3
Nadya <msyoo1682@gmail.com>
2025-05-19 17:18:26 +0300

    games/brain-progression-game.js

--- games/brain-progression-game.js
+++ games/brain-progression-game.js
@@ -1,15 +1,20 @@
 import { gamesStructure } from '../src/index.js'
 import { getRandomNumber } from '../src/index.js'
 
-const questionOfGame = 'What number is missing in the progression?'
-
-const gameProcess = () => {
+const createProgression = () => {
   const seriesOfNumbers = []
   const diffOfProgr = getRandomNumber(1, 10)
   const firstNumber = getRandomNumber(0, 70)
   for (let i = 0; i < getRandomNumber(5, 11); i += 1) {
     seriesOfNumbers[i] = firstNumber + i * diffOfProgr
   }
+  return seriesOfNumbers
+}
+
+const questionOfGame = 'What number is missing in the progression?'
+
+const gameProcess = () => {
+  const seriesOfNumbers = createProgression()
   const hiddenNumber = getRandomNumber(0, seriesOfNumbers.length - 1)
   const correctAnswer = String(seriesOfNumbers[hiddenNumber])
   seriesOfNumbers[hiddenNumber] = '..'
