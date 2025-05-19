17d0a90ca4f1034a12e99d7fff1ed8de78144e13
Nadya <msyoo1682@gmail.com>
2025-05-19 17:32:59 +0300

    games/brain-even-game.js

--- games/brain-even-game.js
+++ games/brain-even-game.js
@@ -1,18 +1,14 @@
 import { gamesStructure } from '../src/index.js'
 import { getRandomNumber } from '../src/index.js'
 
+const defineEven = number => number % 2 === 0 // определение четности числа
+
 const questionOfGame = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 const gameProcess = () => {
-  const a = getRandomNumber(0, 100)
-  const question = a
-  let correctAnswer
-  if (a % 2 !== 0) {
-    correctAnswer = 'no'
-  }
-  if (a % 2 === 0) {
-    correctAnswer = 'yes'
-  }
+  const number = getRandomNumber(0, 100)
+  const question = number
+  const correctAnswer = defineEven(number) ? 'yes' : 'no'
   return {
     question,
     correctAnswer,
