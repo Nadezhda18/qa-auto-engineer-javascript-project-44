2ce5f4820ca35ec516d9e2d2670c9905595759dc
Nadya <msyoo1682@gmail.com>
2025-05-19 17:14:21 +0300

    games/brain-prime-game.js

--- games/brain-prime-game.js
+++ games/brain-prime-game.js
@@ -1,16 +1,21 @@
 import { gamesStructure } from '../src/index.js'
 import { getRandomNumber } from '../src/index.js'
 
-const questionOfGame = 'Answer "yes" if given number is prime. Otherwise answer "no"'
-
-const gameProcess = () => {
+const findDividers = (number) => { // функция для поиска делителей определенного числа
   const dividers = []
-  const number = getRandomNumber(2, 100)
   for (let i = 1; i <= 100; i += 1) {
     if (number % i === 0) {
       dividers.push(i)
     }
   }
+  return dividers
+}
+
+const questionOfGame = 'Answer "yes" if given number is prime. Otherwise answer "no"'
+
+const gameProcess = () => {
+  const number = getRandomNumber(2, 100)
+  const dividers = findDividers(number)
   let correctAnswer = ''
   if (dividers.length === 2 && dividers[0] === 1 && dividers[1] === number) {
     correctAnswer = 'yes'
