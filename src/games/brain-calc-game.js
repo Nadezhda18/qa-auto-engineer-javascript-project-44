ae6eb00b15b1246ad2ae84701ddfca47ab4b63e2
Nadya <msyoo1682@gmail.com>
2025-05-21 19:10:51 +0300

    src/games/brain-calc-game.js

--- src/games/brain-calc-game.js
+++ src/games/brain-calc-game.js
@@ -1,5 +1,5 @@
 import { playGame } from '../index.js'
-import { getRandomNumber } from '../src/index.js'
+import { getRandomNumber } from '../index.js'
 
 const getRandomSign = (signs) => {
   const randomIndex = getRandomNumber(-1, signs.length)
